2bcc22b50432ecc04e976af7cd855147c8fb190c
Yankı Ufuk Dülger <yankiudulger@gmail.com>
2025-02-14 15:26:23 +0300

    Update Shop

--- DriftCarHC Project/Assets/Shop/Shop.cs
+++ DriftCarHC Project/Assets/Shop/Shop.cs
@@ -5,7 +5,25 @@ public class Shop : ScriptableObject
 {
     public Car[] cars;
     public CarController[] carControllers;
+    public GameObject defaultCarModel;
+    public CarController defaultCarController;
+
     public int CarsCount => cars.Length;
+
     public CarController GetCar(int index) => carControllers[index];
-    public void BuyCar(int index) => carControllers[index].isBought = true;
+
+    public void BuyCar(int index)
+    {
+        carControllers[index].isBought = true;
+        UpdateDefaultCar(index);
+    }
+
+    private void UpdateDefaultCar(int index)
+    {
+        // Update the default car's 3D model
+        defaultCarModel = carControllers[index].gameObject;
+
+        // Update the default car's controller stats
+        defaultCarController.InitializeFromCar(cars[index]);
+    }
 }

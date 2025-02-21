fd0bf36ba4c43992b12c44554e37fe42bfd16632
HakanUca <hakanhuca@gmail.com>
2025-02-21 14:32:49 +0300

    new LeanTween package and UI update

---
+++ DriftCarHC Project/Assets/Scenes/UIController.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour
+{
+    [SerializeField] Button settingsButton;
+    [SerializeField] Button settingsPanelBack;
+    [SerializeField] GameObject settingsPanel;
+
+    [SerializeField] Transform settingsButtonClosePosition;
+    [SerializeField] Transform settingsButtonOpenPosition;
+
+    [SerializeField] Transform settingsPanelOpenPosition;
+    [SerializeField] Transform settingsPanelClosePosition;
+
+    void Start()
+    {
+        settingsButton.onClick.AddListener(CloseSettingsButton);
+        settingsPanelBack.onClick.AddListener(OpenSettingsButton);
+    }
+
+    void Update()
+    {
+
+    }
+
+    void CloseSettingsButton()
+    {
+        LeanTween.move(settingsButton.gameObject, settingsButtonClosePosition.position, 0.5f).setEase(LeanTweenType.easeInOutQuad);
+        LeanTween.move(settingsPanel, settingsPanelClosePosition.position, 0.5f).setEase(LeanTweenType.easeInOutQuad);
+    }
+
+    void OpenSettingsButton()
+    {
+        LeanTween.move(settingsButton.gameObject, settingsButtonOpenPosition.position, 0.5f).setEase(LeanTweenType.easeInOutQuad);
+        LeanTween.move(settingsPanel, settingsPanelOpenPosition.position, 0.5f).setEase(LeanTweenType.easeInOutQuad);
+    }
+}

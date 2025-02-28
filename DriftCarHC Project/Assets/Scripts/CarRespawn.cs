bc3b54aeaa40627f5c2ada78ed4970dba0bb574c
kedywd <yusufatakanu@gmail.com>
2025-03-01 01:09:52 +0300

    Update Death Screen

--- DriftCarHC Project/Assets/Scripts/CarRespawn.cs
+++ DriftCarHC Project/Assets/Scripts/CarRespawn.cs
@@ -1,22 +1,61 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
+using TMPro;
 public class CarRespawn : MonoBehaviour
 {
     public Transform respawnPoint;
+    public GameObject gameOverScreen;
+    public TextMeshProUGUI scoreText;
+    private int score = 0;
+    public GameObject[] collectableObjects;
+    private List<GameObject> collectedObjects = new List<GameObject>();
+    void Start()
+    {
 
+        gameOverScreen.SetActive(false);
+        ResetCollectables();
+    }
     void OnTriggerEnter(Collider other)
     {
+
         if (other.CompareTag("Crashable"))
         {
-            RespawnCar();
+
+            ShowGameOverScreen();
         }
     }
 
-    void RespawnCar()
+    public void RespawnCar()
     {
+
         transform.position = respawnPoint.position;
         transform.rotation = respawnPoint.rotation;
+        score = 0;
+        scoreText.text = "Score: " + score;
+        ResetCollectables();
     }
+    void ResetCollectables()
+    {
+
+        foreach (GameObject obj in collectableObjects)
+        {
+            obj.SetActive(true);
+        }
+    }
+    void ShowGameOverScreen()
+    {
+
+        gameOverScreen.SetActive(true);
+    }
+
+    public void RestartGame()
+    {
+
+        gameOverScreen.SetActive(false);
+        RespawnCar();
+    }
+
+
 }

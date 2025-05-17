a77abde50dd7ec0ef71b124e110a89c24f31e6ab
kedywd <yusufatakanu@gmail.com>
2025-05-17 18:14:04 +0300

    update car respawn script

--- DriftCarHC Project/Assets/Scripts/CarRespawn.cs
+++ DriftCarHC Project/Assets/Scripts/CarRespawn.cs
@@ -4,57 +4,100 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 using UnityEngine.SceneManagement;
+
 public class CarRespawn : MonoBehaviour
 {
-    // public Transform respawnPoint;
     private Rigidbody rb;
     public GameObject gameOverScreen;
     public TextMeshProUGUI scoreText;
     private int score = 0;
-    public GameObject[] collectableObjects;
-    private List<GameObject> collectedObjects = new List<GameObject>();
     private CarController carController;
+
+    [Header("Life System")]
+    public int lives = 3;
+    public Image[] lifeIcons;
+
+    private bool isInvincible = false;
+    public float invincibilityDuration = 5f;
+
     void Start()
     {
-
         gameOverScreen.SetActive(false);
         carController = GetComponent<CarController>();
-        // ResetCollectables();
+        UpdateLifeUI();
     }
+
     void OnTriggerEnter(Collider other)
     {
-
-        if (other.CompareTag("Crashable"))
+        Debug.Log("Çarpışma oldu: " + other.name);
+        if (other.CompareTag("Crashable") && !isInvincible)
         {
+            LoseLife();
+        }
+    }
 
+    void LoseLife()
+    {
+        lives--;
+        UpdateLifeUI();
+
+        if (lives <= 0)
+        {
             ShowGameOverScreen();
             StopCar();
         }
+        else
+        {
+            StartCoroutine(StartInvincibility());
+        }
+    }
+
+    IEnumerator StartInvincibility()
+    {
+        isInvincible = true;
+
+        Renderer[] renderers = GetComponentsInChildren<Renderer>();
+        float elapsed = 0f;
+        float blinkInterval = 0.2f;
+
+        while (elapsed < invincibilityDuration)
+        {
+
+            foreach (Renderer r in renderers)
+                r.enabled = false;
+
+            yield return new WaitForSeconds(blinkInterval);
+
+
+            foreach (Renderer r in renderers)
+                r.enabled = true;
+
+            yield return new WaitForSeconds(blinkInterval);
+            elapsed += blinkInterval * 2;
+        }
+
+        isInvincible = false;
     }
 
     public void RespawnCar()
     {
-
-        //  transform.position = respawnPoint.position;
-        //  transform.rotation = respawnPoint.rotation;
         score = 0;
         scoreText.text = "Score: " + score;
+        lives = 3;
+        UpdateLifeUI();
         EnableCar();
-        //ResetCollectables();
     }
-    /*
-    void ResetCollectables()
-    {
 
-        foreach (GameObject obj in collectableObjects)
+    void UpdateLifeUI()
+    {
+        for (int i = 0; i < lifeIcons.Length; i++)
         {
-            obj.SetActive(true);
+            lifeIcons[i].enabled = i < lives;
         }
     }
-    */
+
     void ShowGameOverScreen()
     {
-
         gameOverScreen.SetActive(true);
     }
 
@@ -63,20 +106,17 @@ public class CarRespawn : MonoBehaviour
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         gameOverScreen.SetActive(false);
         RespawnCar();
-
         AudioManager.instance.RestartMusic();
-
     }
+
     public void ShowMainMenu()
     {
         SceneManager.LoadScene("MainMenu");
         gameOverScreen.SetActive(false);
-
     }
 
     void StopCar()
     {
-
         if (carController != null)
         {
             carController.enabled = false;
@@ -85,7 +125,6 @@ public class CarRespawn : MonoBehaviour
 
     void EnableCar()
     {
-
         if (carController != null)
         {
             carController.enabled = true;
